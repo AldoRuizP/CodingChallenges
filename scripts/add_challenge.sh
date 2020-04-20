fdac5042e6228c7b04b8184bae27efdbb9135d1d
Aldo <aldo.ruiz@wizeline.com>
2020-04-20 16:21:26 -0500

    add logic to generate folders including languages and starting files. add pending todos

--- scripts/add_challenge.sh
+++ scripts/add_challenge.sh
@@ -7,6 +7,18 @@ function print_question() {
   printf "\n\033[1m$1\033[0m\n"
 }
 
+function create_js_folder() {
+  mkdir JavaScript && cd JavaScript && touch answer.js
+  echo -e "const runTests = require('../../../RunTests/JavaScript/index')\n\nfunction myFunction(){\n  /* Fill this... */\n}\n\nrunTests( myFunction )" >> answer.js
+  cd ../
+}
+
+function create_python_folder(){
+  mkdir Python && cd Python && touch answer.py
+  echo -e "import sys\nsys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')\nfrom run_test import run_tests\n\ndef my_function():\n  # Fill this..\n  return\n\nrun_tests( my_function )" >>  answer.py
+  cd ../
+}
+
 function init() {
   printf "\nUse this program to add a new challenge\n";
 
@@ -28,10 +40,12 @@ function init() {
   print_question "\nEnter the constrains for the new challenge (or leave empty if there are non):"
   read -p "" challenge_constrains;
 
-  cd src/$difficulty && mkdir $challenge_name && cd $challenge_name && touch README.md
+  cd src/$difficulty && mkdir $challenge_name && cd $challenge_name && touch README.md && touch tests
   echo -e "## Description\n\n$challenge_description\n\n## Starting Point\n\n$challenge_starting_point\n\n## Constrains\n\n$challenge_constrains" >> README.md
+
+  create_js_folder
+  create_python_folder
+  
 }
-
-
 
 init
