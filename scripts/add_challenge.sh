74e749597dfb1e17b95c7a036edcd6a9670b04b7
Aldo <aldo.ruiz@wizeline.com>
2020-04-21 11:08:52 -0500

    Added validation for folder names

--- scripts/add_challenge.sh
+++ scripts/add_challenge.sh
@@ -11,6 +11,10 @@ function print_log(){
   printf "\n\033[0;32m$1\033[0m"
 }
 
+function print_error(){
+  printf "\n\033[0;31m$1\033[0m\n" > /dev/stderr 
+}
+
 function create_js_folder() {
   mkdir JavaScript && cd JavaScript && touch answer.js
   echo -e "const runTests = require('../../../RunTests/JavaScript/index')\n\nfunction myFunction(){\n  /* Fill this... */\n}\n\nrunTests( myFunction )" >> answer.js
@@ -36,27 +40,44 @@ function read_multiline_input() {
   echo "$tmp"
 }
 
+function validate_challenge_name() {
+  unset folder_name
+  while :
+  do
+    read folder_name
+    if [[ ! -d "./src/$difficulty/$folder_name" ]]; then break
+    else print_error "There's already a challenge with this name. Please enter a different one\n"; fi
+  done
+  echo "$folder_name"
+}
+
 function init() {
   printf "\nUse this program to add a new challenge\n";
 
+  # Capture difficulty
   print_question "First, select the appropriate difficulty level of the new challenge:";
-
   select_option "${difficulty_options[@]}"
   choice=$?
-  local difficulty="${difficulty_options[choice]}"
+  difficulty="${difficulty_options[choice]}"
 
+  # Capture challenge name
   print_question "Enter a name for your challenge, (use PascalCase):"
-  read -p "" challenge_name;
+  challenge_name=$(validate_challenge_name $difficulty)
+  print_log "The given name \"$challenge_name\" is available!.\n"
   
+  # Capture description
   print_question "Enter the description of the new challenge: (type \"DONE\" to finish input)"
   challenge_description=$(read_multiline_input)
 
+  # Capture starting point
   print_question "\nEnter the starting point of the new challenge, (use markdown syntax and type \"DONE\" to finish input):"
   challenge_starting_point=$(read_multiline_input)
 
+  # Capture constrains
   print_question "\nEnter the constrains for the new challenge (or leave empty if there are non):"
   read -p "" challenge_constrains;
 
+  # Create folders and files
   cd src/$difficulty && mkdir $challenge_name  && cd $challenge_name
   print_log "Created folder structure"
   touch README.md 
