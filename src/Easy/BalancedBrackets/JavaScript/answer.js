e7084152e8fa8b2d3f040a0763ba1fd0de989f04
Aldo <aldo.ruiz@wizeline.com>
2020-04-27 11:22:13 -0500

    Add balanced brackets challenge and JS solution

---
+++ src/Easy/BalancedBrackets/JavaScript/answer.js
@@ -0,0 +1,29 @@
+const runTests = require('../../../RunTests/JavaScript/index')
+
+const bracketsMap = {
+  '(' : ')',
+  '[' : ']',
+  '{' : '}'
+}
+
+/**
+ * A function that verifies if a string of brackets is balanced
+ * @param { string } inputString - The string to validate
+ * @returns { boolean } - True or false if the string has balanced brackets
+ */
+function myFunction( inputString ){
+  const bracketStack = []
+
+  inputString.split('').forEach( char => {
+    const lastItemInStack = bracketStack.pop()
+    const closingForLastItem = bracketsMap[lastItemInStack]
+    if ( char !== closingForLastItem ){
+      if ( lastItemInStack ) bracketStack.push(lastItemInStack)
+      bracketStack.push(char)
+    }
+  })
+
+  return bracketStack.length === 0
+}
+
+runTests( myFunction )
---
+++ src/Easy/BalancedBrackets/README.md
@@ -0,0 +1,22 @@
+## Description
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+- Open brackets are closed by the same type of brackets.
+- Open brackets are closed in the correct order.
+- Note that an empty string is also considered valid.
+
+## Starting Point
+
+``` python
+def is_valid(s):
+# Fill this
+
+print (is_valid('()(){}'))
+# true
+
+```
+
+## Constrains
+
+None.
