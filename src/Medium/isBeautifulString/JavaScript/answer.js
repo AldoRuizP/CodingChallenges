26c47a391150c8028906a750b299abd93f6d82b8
Aldo <aldo.ruiz@wizeline.com>
2020-04-27 09:14:08 -0500

    add jsdocs for answer

--- src/Medium/isBeautifulString/JavaScript/answer.js
+++ src/Medium/isBeautifulString/JavaScript/answer.js
@@ -1,6 +1,11 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 const FIRST_ASCII_VALUE = 96
 
+/**
+ * A function that determines if a given string is beautiful
+ * @param { string } inputString - The string to analyze
+ * @returns { boolean } - True or false if the given string is beautiful
+ */
 function myFunction(inputString) {
 
   charMap = new Map()
@@ -15,12 +20,22 @@ function myFunction(inputString) {
 }
 
 
+/**
+ * A function that determines if the values an array of characters are in a strict sequence and don't skip any char in between
+ * @param { char[] } keys - An array of all the characters that appear an in the given string
+ * @returns { boolean } - True or false if the characters are in a proper sequence and don't skip any char in between
+ */
 function checkAlphabeticalOrder(keys) {
   const lastChar = keys[ keys.length - 1 ]
   const expectedLastChar = String.fromCharCode( keys.length + FIRST_ASCII_VALUE )
   return lastChar  === expectedLastChar
 }
 
+/**
+ * A function that determines if the values of an array of integers are in descendent order
+ * @param { integer[] } values - An array of integers with the number of occurrences of each character in the string
+ * @returns { boolean } - True or false if the given array of integers in descendent order
+ */
 function checkDescendentOrder(values) {
   const sortedValues = values.slice().sort( (a, b) =>  b - a ) 
   const areDifferent = sortedValues.some( ( sorted, index ) => sorted !== values[ index ] )
