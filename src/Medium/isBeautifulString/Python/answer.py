eb2c6233b9962fab5f744c6185286ee8ab98c009
Aldo <aldo.ruiz@wizeline.com>
2020-04-27 09:15:36 -0500

    Add pydocs to answer

--- src/Medium/isBeautifulString/Python/answer.py
+++ src/Medium/isBeautifulString/Python/answer.py
@@ -5,6 +5,11 @@ from run_test import run_tests
 
 from string import ascii_lowercase
 
+"""
+Determine if a given string is beautiful
+:param input_string: A string to analyze if its beautiful or not
+:return Boolean indicating if the string is beautiful or not
+"""
 def my_function( input_string ):
   for i, char in enumerate( ascii_lowercase[1:], 1):
     if input_string.count( char ) >  input_string.count( ascii_lowercase[ i-1 ] ):
