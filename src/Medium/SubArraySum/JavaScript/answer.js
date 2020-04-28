a8f4bd8e6a37ece359ce47ebfc16e3ffd4f6f134
Aldo <aldo.ruiz@wizeline.com>
2020-04-28 11:29:50 -0500

    add js docs

--- src/Medium/SubArraySum/JavaScript/answer.js
+++ src/Medium/SubArraySum/JavaScript/answer.js
@@ -1,5 +1,10 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
+/**
+ * Find a subarray that sums up to the first item in the original array and returns it
+ * @param { integer[] } numbers - An array of integers, where the first item is the target and the rest contains a subarray which sums up to target
+ * @returns { integer[] } subArray - The subarray which sums up to target
+ */
 function subArraySum( numbers ){
 
   const target = numbers.shift()
@@ -8,20 +13,13 @@ function subArraySum( numbers ){
   let index = 0
 
   while ( currentSum !== target ) {
-
     const currentNumber = numbers[index++]
     currentSum += currentNumber
     subArray.push( currentNumber )
-
-    while( currentSum > target ) {
-      currentSum -= subArray.shift()
-    }
-
+    while( currentSum > target ) currentSum -= subArray.shift()
   }
 
   return subArray
 }
-
-//console.log( subArraySum( [ 14, 1, 3, 2, 5, 7, 2] ) ) // [ 2, 5, 7 ]
 
 runTests( subArraySum )
