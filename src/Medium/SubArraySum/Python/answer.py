f48c64775f8024fff999961a053ca5b06700bb72
Aldo <aldo.ruiz@wizeline.com>
2020-04-28 11:27:31 -0500

    Add python solution to subarray sum

--- src/Medium/SubArraySum/Python/answer.py
+++ src/Medium/SubArraySum/Python/answer.py
@@ -2,8 +2,20 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+
+def my_function( numbers ):
+  target = numbers.pop(0)
+  low_index = 0
+  high_index = 1
+  current_sum = 0
+
+  while current_sum != target:
+    high_index += 1
+    current_sum = sum( numbers[ low_index:high_index ] )
+    while current_sum > target:
+      low_index += 1
+      current_sum = sum( numbers[ low_index:high_index ] )
+    
+  return numbers[low_index:high_index]
 
 run_tests( my_function )
