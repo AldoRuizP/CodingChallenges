f6686c0d2770a6b7f81a22a84053b5b30c0df453
Aldo <aldo.ruiz@wizeline.com>
2020-04-29 14:21:19 -0500

    missing word permutation

--- src/Hard/FindConcatenations/JavaScript/answer.js
+++ src/Hard/FindConcatenations/JavaScript/answer.js
@@ -12,9 +12,6 @@ function findCompoundWords( words ){
 
   const possibleCombinations = findPossibleSums( [...wordsMappedByLength.keys()] )
 
-
-
-
 }
 
 function findPossibleSums( numbers ){
@@ -37,60 +34,17 @@ function findPossibleSums( numbers ){
 function findCombinationsUtil( arr, index, num, reducedNum, answers ){
 
   if (reducedNum < 0 ) return answers
-  
-  if (reducedNum === 0 ){
-    answers.push( arr.slice( 0, index ) )
-    return
-  }
-
+  if (reducedNum === 0 ) return answers.push( arr.slice( 0, index ) )
   let prev = index === 0 ? 1 : arr[index - 1]
 
   for( let k = prev; k < num + 1; k++ ){
     arr[index] = k
     findCombinationsUtil( arr, index + 1, num, reducedNum - k, answers  )
   }
+  
 }
 
 findCompoundWords( [, "a", "tech", "lead", "techlead", "cat", "cats", "dog", "catsdog"] )
-
-
-/*
-def findCombinationsUtil(arr, index, num, reducedNum): 
-  
-  # Base condition 
-  if (reducedNum < 0): 
-    return; 
-
-  # If combination is found, print it 
-  if (reducedNum == 0): 
-    for i in range(index): 
-      print(arr[i], end = " "); 
-    return; 
-
-  # Find the previous number stored in arr[]. It helps in maintaining increasing order 
-  prev = 1 if(index == 0) else arr[index - 1]; 
-
-  # note loop starts from previous number i.e. at array location index - 1 
-  for k in range(prev, num + 1): 
-    arr[index] = k; 
-    findCombinationsUtil(arr, index + 1, num, reducedNum - k); 
-  
-# Function to find out all  
-# combinations of positive numbers  
-# that add upto given number. 
-# It uses findCombinationsUtil()  
-def findCombinations(n): 
-      
-    # array to store the combinations 
-    # It can contain max n elements 
-    arr = [0] * n; 
-  
-    # find all combinations 
-    findCombinationsUtil(arr, 0, n, n); 
-*/
-
-
-
 
 
 //runTests( myFunction )
