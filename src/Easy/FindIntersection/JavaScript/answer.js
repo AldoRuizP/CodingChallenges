c215cdae762347bfa6326ae0f5cc3eed7c4b9e67
Aldo <aldo.ruiz@wizeline.com>
2020-05-04 12:13:37 -0500

    Add jsdocs

--- src/Easy/FindIntersection/JavaScript/answer.js
+++ src/Easy/FindIntersection/JavaScript/answer.js
@@ -1,5 +1,10 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
+/**
+ * A function that scans two array in order to find all the intersected elements they have
+ * @param { string[] } stringArray - An array with two strings, which represent the arrays to scan
+ * @returns { string | boolean } - A string listing all the intersected elements, or a false value if there are none
+ */
 function findIntersection( stringArray ){
   const firstArray  = stringArray[0].split(', ')
   const secondArray = stringArray[1].split(', ')
