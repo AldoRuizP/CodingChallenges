f517908ca42695734c37f11f2a2bbea9f2938b31
Aldo <aldo.ruiz@wizeline.com>
2020-05-04 13:42:02 -0500

    first python approach

--- src/Easy/FindIntersection/Python/answer.py
+++ src/Easy/FindIntersection/Python/answer.py
@@ -2,8 +2,10 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def find_intersection( str_array ):
+  array_1 = map(int, str_array[0].split(', '))
+  array_2 = map(int, str_array[1].split(', '))
+  matches =  repr ( set(array_1) & set(array_2) )
+  return matches.replace(' ', '').replace('{','').replace('}','') if matches else False
 
-run_tests( my_function )
+run_tests( find_intersection )
