2382ddc1e79aac9cb6d85480bbcd05da51e8a3b2
Aldo <aldo.ruiz@wizeline.com>
2020-05-05 09:00:02 -0500

    Fix python answer

--- src/Easy/FindIntersection/Python/answer.py
+++ src/Easy/FindIntersection/Python/answer.py
@@ -5,7 +5,9 @@ from run_test import run_tests
 def find_intersection( str_array ):
   array_1 = map(int, str_array[0].split(', '))
   array_2 = map(int, str_array[1].split(', '))
-  matches =  repr ( set(array_1) & set(array_2) )
-  return matches.replace(' ', '').replace('{','').replace('}','') if matches else False
+  matches = set(array_1) & set(array_2)
+  sorted_matches = sorted(matches)
+  matches_string =  repr ( sorted_matches )
+  return matches_string.replace(' ', '').replace('[','').replace(']','') if matches else False
 
 run_tests( find_intersection )
