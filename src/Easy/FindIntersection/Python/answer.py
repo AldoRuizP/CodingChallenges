7e23d6a017316a207ff52b5b41c9ae07debe7fa0
Aldo <aldo.ruiz@wizeline.com>
2020-05-05 09:04:36 -0500

    add pydocs

--- src/Easy/FindIntersection/Python/answer.py
+++ src/Easy/FindIntersection/Python/answer.py
@@ -3,11 +3,29 @@ sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
 def find_intersection( str_array ):
-  array_1 = map(int, str_array[0].split(', '))
-  array_2 = map(int, str_array[1].split(', '))
+  """ A function that scans two stringified arrays and returns a new string with all the matches between the two arrays
+
+  Arguments:
+      str_array { string[] } -- An array of strings with two elements. Each element is a stringified array of numbers
+
+  Returns:
+      [ string ] -- A string with all the matching elements that the two arrays share
+  """
+  array_1 = parse_string_array( str_array[0] )
+  array_2 = parse_string_array( str_array[1] )
   matches = set(array_1) & set(array_2)
-  sorted_matches = sorted(matches)
-  matches_string =  repr ( sorted_matches )
-  return matches_string.replace(' ', '').replace('[','').replace(']','') if matches else False
+  sorted_matches = repr ( sorted(matches) )
+  return sorted_matches.replace(' ', '').replace('[','').replace(']','') if matches else False
+
+def parse_string_array( string_array ):
+  """ A function that receives a stringified array of numbers, and parses it into a proper number array
+
+  Arguments:
+      string_array { string } -- A stringified array of numbers
+
+  Returns:
+      [ nubmer[]  -- An array of numbers
+  """
+  return map(int, string_array.split(', '))
 
 run_tests( find_intersection )
