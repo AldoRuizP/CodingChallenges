d93cb7905c4378b6fe748f26380e578920fda8a4
Aldo <aldo.ruiz@wizeline.com>
2020-05-06 09:06:51 -0500

    add jsdocs

--- src/Medium/questionMarks/JavaScript/answer.js
+++ src/Medium/questionMarks/JavaScript/answer.js
@@ -1,5 +1,11 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
+/**
+ * A function that determines if a string of numbers, letters and question marks has exactly three question marks between each pair of 
+ * numbers that add up to 10
+ * @param { string } str - The input string to validate
+ * @returns { boolean } - True or false if all the pairs of numbers that add up to 10, have exactly three question marks in between them
+ */
 function questionMarks(str) {
 
   let has10Sum = false
