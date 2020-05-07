ff17d90076f3c1b8e600e92ea9b0d580ca8fdc1b
Aldo <aldo.ruiz@wizeline.com>
2020-05-07 09:48:27 -0500

    added js solution

--- src/Easy/LetterChanges/JavaScript/answer.js
+++ src/Easy/LetterChanges/JavaScript/answer.js
@@ -1,7 +1,30 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that shifts all the letters of a string to its next letter in the alphabet, and then uppercase all the vowels
+ * @param { string } str - The string to convert
+ * @return { string } - The converted string
+ */
+function letterChanges( str ){
+  return str.split('').map( c => shiftAndConvertLetter( c )).join('')
 }
 
-runTests( myFunction )
+/**
+ * Auxiliary function to shift and capitalize a letter 
+ * @param { char } letter - The letter to be shifted and capitalized
+ * @returns { char } = The shifted and capitalized letter
+ */
+function shiftAndConvertLetter( letter ){
+  let shiftedLetter = letter
+
+  if ( letter >= 'a' && letter <= 'z' ) {
+    const nextRawAscii = letter.charCodeAt() + 1
+    const nextAscii = ( nextRawAscii === 123 )  ? 97 : nextRawAscii
+    shiftedLetter = String.fromCharCode( nextAscii )
+  }
+
+  return /[aeiou]/.test(shiftedLetter) ? shiftedLetter.toUpperCase() : shiftedLetter
+}
+
+
+runTests( letterChanges )
