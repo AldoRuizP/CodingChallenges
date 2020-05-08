f8dcb80b8b22d5cbe9fac34d5a58df0d9d34a0e8
Aldo <aldo.ruiz@wizeline.com>
2020-05-08 10:55:40 -0500

    add python solution

--- src/Easy/longestWord/Python/answer.py
+++ src/Easy/longestWord/Python/answer.py
@@ -2,8 +2,20 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
 
-run_tests( my_function )
+from string import ascii_letters
+whitelist = set ( ascii_letters + ' ' + '1234567890' )
+
+def longest_word( sen ):
+  """ A function that finds the longest word in a string, ignoring special characters
+
+  Arguments:
+      sen { string } -- The word to analyze
+
+  Returns:
+      [ string ] -- The first longest word in the string
+  """
+  words = ''.join( l for l in sen if l in whitelist ).split()
+  return max( words , key=len)
+
+run_tests( longest_word )
