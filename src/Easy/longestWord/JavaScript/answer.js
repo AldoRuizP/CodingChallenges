c1714ca0e001ad1f0085ec4a2f33954a0437351c
Aldo <aldo.ruiz@wizeline.com>
2020-05-08 10:18:54 -0500

    Added js solution

--- src/Easy/longestWord/JavaScript/answer.js
+++ src/Easy/longestWord/JavaScript/answer.js
@@ -1,7 +1,12 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that finds the longest word in a string, ignoring special characters
+ * @param { string } sen - The word to analyze
+ * @returns { string } - The first longest word in the string
+ */
+function longestWord( sen ){
+  return sen.replace( /[^1-9a-zA-Z ]/g, '').split(' ').reduce( (a, b) => a.length < b.length ? b : a, '')
 }
 
-runTests( myFunction )
+runTests( longestWord )
