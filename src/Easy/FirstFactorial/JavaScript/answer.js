5277d104562ab1db726b6169cfa81b8993d79b05
Aldo <aldo.ruiz@wizeline.com>
2020-05-11 09:49:30 -0500

    add js solution

--- src/Easy/FirstFactorial/JavaScript/answer.js
+++ src/Easy/FirstFactorial/JavaScript/answer.js
@@ -1,7 +1,12 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A recursive function that returns the factorial of a given number
+ * @param { number } n  - The given number to calculate it's factorial
+ * @return { number } - The product of the current number times the previous number. Defaulted to 1 when n equals zero
+ */
+function firstFactorial( n ){
+  return n === 0 ? 1 :  n * firstFactorial( n - 1) 
 }
 
-runTests( myFunction )
+runTests( firstFactorial )
