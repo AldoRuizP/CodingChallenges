bbdca52bce395b8500d2aee260dae11a5e896379
Aldo <aldo.ruiz@wizeline.com>
2020-05-11 09:52:54 -0500

    add python solution

--- src/Easy/FirstFactorial/Python/answer.py
+++ src/Easy/FirstFactorial/Python/answer.py
@@ -2,8 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def first_factorial( n ):
+  """ A recursive function that calculates the factorial of a given number
 
-run_tests( my_function )
+  Arguments:
+      n { number } -- The given number to calculate its factorial
+
+  Returns:
+      [ number ] -- The product of the current number times the previous number. Defaulted to 1 when n equals zero
+  """
+  return 1 if n == 0 else n * first_factorial( n - 1 )
+
+run_tests( first_factorial )
