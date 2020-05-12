1b519d527a5c8af864a00b53410653336df1a561
Aldo <aldo.ruiz@wizeline.com>
2020-05-12 09:03:11 -0500

    Add python solution

--- src/Easy/ReverseString/Python/answer.py
+++ src/Easy/ReverseString/Python/answer.py
@@ -2,8 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def reverse_string( str ):
+  """ A function that receives a string and reverses it
 
-run_tests( my_function )
+  Arguments:
+      str { string } -- The string to reverse
+
+  Returns:
+      [string ] -- The reversed string
+  """
+  return str[::-1]
+
+run_tests( reverse_string )
