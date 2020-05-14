d4f4f8e33a5890944f3f4778c84a41ff7892daa5
Aldo <aldo.ruiz@wizeline.com>
2020-05-14 09:16:09 -0500

    Add python solution

--- src/Easy/BinaryReversal/Python/answer.py
+++ src/Easy/BinaryReversal/Python/answer.py
@@ -2,8 +2,18 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def binary_reverse( string ):
+  """ A function that receives a stringified positive integer and returns its binary reversal integer representation
 
-run_tests( my_function )
+  Arguments:
+      string { str } -- The string representing a positive integer 
+
+  Returns:
+      [integer] -- The integer which correspond to the binary reversal of the input
+  """
+  n = bin(int(string))[2:]
+  missing_zeroes = 8 - (len(n) % 8) if len(n) % 8 != 0 else 0
+  k = (( '0' * missing_zeroes ) + n)[::-1]
+  return int( k, 2 )
+
+run_tests( binary_reverse )
