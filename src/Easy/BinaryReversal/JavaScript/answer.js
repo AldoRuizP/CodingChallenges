d2944cc4d127855d12d5f8ef1d06b3633a001723
Aldo <aldo.ruiz@wizeline.com>
2020-05-14 09:00:46 -0500

    Add js solution

--- src/Easy/BinaryReversal/JavaScript/answer.js
+++ src/Easy/BinaryReversal/JavaScript/answer.js
@@ -1,7 +1,17 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that receives a stringified positive integer and returns its binary reversal integer representation
+ * @param { string } str - The string representing a positive integer 
+ * @returns { integer } - The integer which correspond to the binary reversal of the input 
+ */
+function binaryReversal( str ){
+  let binary = parseInt(str).toString(2)
+  const missingPrefix = binary.length % 8 === 0 ? 0 : 8 - binary.length % 8
+  binary =  '0'.repeat(missingPrefix) + binary
+  const binaryReversed =  binary.split('').reverse().join('')
+  return parseInt(binaryReversed, 2)
 }
 
-runTests( myFunction )
+runTests( binaryReversal )
+
