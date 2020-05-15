90b096636427ea3c42abd1d6100277ac42916734
Aldo <aldo.ruiz@wizeline.com>
2020-05-15 09:25:01 -0500

    add python solution

--- src/Easy/LongestNonRepeatingSubstring/Python/answer.py
+++ src/Easy/LongestNonRepeatingSubstring/Python/answer.py
@@ -2,8 +2,27 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def my_function( string ):
+  """ A function that returns the longest substring of nonrepeating characters in a string
+
+  Arguments:
+      string { str } -- Theinput string to analyze
+
+  Returns:
+      [ str ] -- The length of the longest substring of nonrepeating characters
+  """
+  
+  str_list = []
+  max_length = 0
+  
+  for x in string:
+    if x in str_list:
+      str_list = str_list[ str_list.index(x) + 1 : ]
+            
+    str_list.append(x) 
+    max_length = max(max_length, len(str_list))
+        
+  return max_length
+
 
 run_tests( my_function )
