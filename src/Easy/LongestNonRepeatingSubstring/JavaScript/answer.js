acbe2b893c08a7a9612c22f22f0666401c68ff2e
Aldo <aldo.ruiz@wizeline.com>
2020-05-15 09:07:53 -0500

    added longest substring challenge

---
+++ src/Easy/LongestNonRepeatingSubstring/JavaScript/answer.js
@@ -0,0 +1,24 @@
+const runTests = require('../../../RunTests/JavaScript/index')
+
+/**
+ * A function that given a string, returns the longest substring of non repeating characters
+ * @param { string } str - The input string to analyze
+ * @returns { string } - The longest substring of non repeating characters
+ */
+function longestSubString( str ){
+  let currentLongestSet = new Set()
+  const length =  str.split('').reduce( ( longest, char ) => {
+    
+    if ( currentLongestSet.has(char) ){
+      if ( currentLongestSet.size > longest ) longest = currentLongestSet.size
+      currentLongestSet.clear()
+    }
+
+    currentLongestSet.add(char)
+    return longest
+  }, 0 )
+
+  return length || str.length
+}
+
+runTests( longestSubString )
---
+++ src/Easy/LongestNonRepeatingSubstring/README.md
@@ -0,0 +1,18 @@
+## Description
+
+Given a string, find the length of the longest substring without repeating characters.
+
+## Starting Point
+
+``` python
+class Solution:
+def lengthOfLongestSubstring(self, s):
+# Fill this in.
+
+print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+# 10
+```
+
+## Constrains
+
+Find a solution using linear time
