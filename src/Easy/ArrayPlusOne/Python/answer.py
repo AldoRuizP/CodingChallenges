468542dd108f3b4bf58442a12f311a4918efa919
Aldo <aldo.ruiz@wizeline.com>
2020-05-19 10:04:53 -0500

    add python solution

--- src/Easy/ArrayPlusOne/Python/answer.py
+++ src/Easy/ArrayPlusOne/Python/answer.py
@@ -2,8 +2,17 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def array_plus_one( arr ):
+  """  A function that receives an array of integers. Each element in the array represent a digit in a larger number.
+  The function returns an array representing the number after adding one
 
-run_tests( my_function )
+  Arguments:
+      arr { int[] } --  An array of numbers that represent a larger integer
+
+  Returns:
+      int[] -- An array of numbers that represent the given large number after adding one
+  """
+  number = int(''.join( map( str,arr ) ) ) + 1
+  return [int(i) for i in str(number)]
+
+run_tests( array_plus_one )
