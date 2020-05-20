56ee127e9751906dfb0e6b3352e792b5285e5608
Aldo <aldo.ruiz@wizeline.com>
2020-05-20 10:21:11 -0500

    Add python solution

--- src/Medium/ColorSorting/Python/answer.py
+++ src/Medium/ColorSorting/Python/answer.py
@@ -2,8 +2,32 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def sort_colors( colors ):
+  """ A function that sorts an array of integers, in-place and in a single pass
 
-run_tests( my_function )
+  Arguments:
+      colors { int[] } -- An array of integers which may be 0, 1, or 2. 
+
+  Returns:
+      [ int[] ] -- A sorted array of integers, which may be 0, 1, or 2. 
+  """
+
+  left = 0
+  mid = 0
+  right = len( colors ) - 1
+
+  while mid <= right:
+
+    if colors[mid] == 0:
+      colors[left], colors[mid] = colors[mid], colors[left]
+      left += 1
+      mid += 1
+    elif colors[mid] == 1:
+      mid += 1
+    else: 
+      colors[mid], colors[right] = colors[right], colors[mid]
+      right -= 1
+   
+  return colors
+
+run_tests( sort_colors )
