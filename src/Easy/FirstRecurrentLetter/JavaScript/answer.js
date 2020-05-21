7db1b7d0de650cf9da06a22506db1e9add2ba20f
Aldo <aldo.ruiz@wizeline.com>
2020-05-21 10:03:03 -0500

    Add js solution

--- src/Easy/FirstRecurrentLetter/JavaScript/answer.js
+++ src/Easy/FirstRecurrentLetter/JavaScript/answer.js
@@ -1,7 +1,22 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that scans a given string in order to determine what is its first recurrent letter
+ * @param { string } str - The string to scan in order to fin the first repeated character
+ * @returns { string } = a string with the first repeated character
+ */
+function firstRecurrentLetter( str ){
+
+  const lettersSet = new Set()
+  let repeated = ''
+
+  for ( let i = 0; i < str.length; i++){
+    const letter = str.charAt(i)
+    if ( !repeated && lettersSet.has(letter) ) return letter
+    else lettersSet.add( letter )
+  }
+
+  return  'None'
 }
 
-runTests( myFunction )
+runTests( firstRecurrentLetter )
