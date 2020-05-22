bc302dcce4f3f6a94a465a8f588c8c0e243f6dc7
Aldo <aldo.ruiz@wizeline.com>
2020-05-22 11:55:31 -0500

    add python solution

--- src/Easy/MaximalAdjacentDifference/Python/answer.py
+++ src/Easy/MaximalAdjacentDifference/Python/answer.py
@@ -2,8 +2,17 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def array_maximal_adjacent_difference( arr ):
+  """ A function that returns the max difference between any of the elements in the array
 
-run_tests( my_function )
+  Arguments:
+      arr { number[] } -- An array of numbers 
+
+  Returns:
+      number -- The max difference between any of its adjacent element
+  """
+  length = len(arr) - 1
+  diffs = [ abs( arr[i] - arr[i+1] ) for i in range( length ) ]
+  return max(diffs)
+
+run_tests( array_maximal_adjacent_difference )
