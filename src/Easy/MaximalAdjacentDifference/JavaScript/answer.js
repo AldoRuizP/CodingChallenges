cf33e633f0e2f5678f6bc779c73b3600a0c04c9e
Aldo <aldo.ruiz@wizeline.com>
2020-05-22 11:55:22 -0500

    add js solution

--- src/Easy/MaximalAdjacentDifference/JavaScript/answer.js
+++ src/Easy/MaximalAdjacentDifference/JavaScript/answer.js
@@ -1,7 +1,19 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that returns the max difference between any of the elements in the array
+ * @param { number[] } inputArray - An array of numbers 
+ * @returns { number } maxDiff - The max difference between any of its adjacent element
+ */
+function arrayMaximalAdjacentDifference(inputArray) {
+  let maxDiff = 0
+
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    const currentDiff = Math.abs(inputArray[i] - inputArray[i + 1])
+    maxDiff = Math.max(currentDiff, maxDiff)
+  }
+
+  return maxDiff
 }
 
-runTests( myFunction )
+runTests(arrayMaximalAdjacentDifference)
