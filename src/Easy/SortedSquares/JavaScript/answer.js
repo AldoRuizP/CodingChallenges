3449ae103aaa05336024695cddc15115b4094124
Aldo <aldo.ruiz@wizeline.com>
2020-05-25 09:36:45 -0500

    add js solution

--- src/Easy/SortedSquares/JavaScript/answer.js
+++ src/Easy/SortedSquares/JavaScript/answer.js
@@ -1,7 +1,15 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that squares all the numbers in an array, and returns them sorted
+ * Array.prototype.map() -> O(n)
+ * Array.prototype.sort() -> O(n log n) 
+ * Total: O(n) + O( n log n ) = O(n)
+ * @param { number[] } numbers - An array or sorted numbers which can be positive or negative
+ * @returns { number[] } - An array of positive numbers
+ */
+function sortedSquares( numbers ){
+  return numbers.map( num => Math.pow(num, 2)).sort( (a,b) => a - b )
 }
 
-runTests( myFunction )
+runTests( sortedSquares )
