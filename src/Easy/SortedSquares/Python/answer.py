57e6b7ded8440bf4a04e356c991601ef6d62eb4d
Aldo <aldo.ruiz@wizeline.com>
2020-05-25 09:52:48 -0500

    Add python solution

--- src/Easy/SortedSquares/Python/answer.py
+++ src/Easy/SortedSquares/Python/answer.py
@@ -2,8 +2,17 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def sorted_squares( numbers ):
+  """ A function that squares all the numbers in an array, and returns them sorted
 
-run_tests( my_function )
+  Arguments:
+      numbers { int[] } -- An array or sorted numbers which can be positive or negative
+
+  Returns:
+      int[] --  An array of positive numbers
+  """
+  squared_numbers = [ number**2 for number in numbers ]
+  squared_numbers.sort()
+  return squared_numbers
+
+run_tests( sorted_squares )
