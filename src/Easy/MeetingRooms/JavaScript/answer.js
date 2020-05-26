d919a4fe1346da9feff727269c8a589f5b1b3a71
Aldo <aldo.ruiz@wizeline.com>
2020-05-26 10:21:31 -0500

    add js solution

--- src/Easy/MeetingRooms/JavaScript/answer.js
+++ src/Easy/MeetingRooms/JavaScript/answer.js
@@ -1,7 +1,39 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that determines how many rooms will be needed to schedule all the given meetings
+ * @param { number[][] } meetings - An array of meetings. Each meeting is represented by an array of two elements determining start and end time
+ * @returns { number } rooms - The number of room meetings that will be needed to succeed scheduling them all
+ */
+function meetingRooms( meetings ){
+  let rooms = 1
+
+  for( let i = 0; i < meetings.length; i++ ){
+
+    let collisions = 0
+
+    for ( let j = 0; j < meetings.length; j++ ){
+      const meetingsOverlap = areMeetingsColliding( meetings[i], meetings[j] )
+      if ( meetingsOverlap ) collisions++
+    }
+
+    if ( collisions > 1 ) rooms++
+  }
+
+  return rooms
 }
 
-runTests( myFunction )
+/**
+ * Helper function that determines if two meetings are colliding based on their schedules
+ * @param { number[] } meetingA - An array of two numbers representing the start and end time of a meeting
+ * @param { number[] } meetingB - An array of two numbers representing the start and end time of a meeting
+ * @returns { boolean } - True or false if the two meetings collide
+ */
+function areMeetingsColliding( meetingA, meetingB ){
+  for( let i = meetingA[0]; i < meetingA[1] - 1; i++ ){
+    if ( i === meetingB[0] || i === meetingB[1] - 1 ) return true
+  }
+  return false
+}
+
+runTests( meetingRooms )
