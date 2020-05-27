32aead5a04da80c7e1b5811bebc70f72cb5186f7
Aldo <aldo.ruiz@wizeline.com>
2020-05-27 10:11:41 -0500

    add python solution

--- src/Easy/IntersectionOfLists/Python/answer.py
+++ src/Easy/IntersectionOfLists/Python/answer.py
@@ -2,8 +2,16 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def list_intersection( lists ):
+  """ A function that finds an intersection between three lists
 
-run_tests( my_function )
+  Arguments:
+      lists { number[][] } --  A list of lists. The inner lists represent a sorted list
+
+  Returns:
+      number -- The intersection of the three lists, or -1 if there's no intersection
+  """
+  combined_list = list( set( lists[0] ) & set( lists[1] ) & set( lists[2] ) ) or [ -1 ]
+  return combined_list[0]
+
+run_tests( list_intersection )
