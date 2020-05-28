a03627bc7d171497e3328efe2730b8f5c4a6e858
Aldo <aldo.ruiz@wizeline.com>
2020-05-28 10:26:41 -0500

    ad python solution

--- src/Easy/ReverseWords/Python/answer.py
+++ src/Easy/ReverseWords/Python/answer.py
@@ -2,8 +2,17 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
 
-run_tests( my_function )
+def reverse_words( str ):
+  """ A function that reverses the words in a string while keeping word order
+
+  Arguments:
+      str { string } --  The string to process
+
+  Returns:
+      string  -- The original string with each word reversed
+  """
+  reversed_words = [ word[::-1] for word in str.split() ]
+  return ' '.join(reversed_words)
+
+run_tests( reverse_words )
