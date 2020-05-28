848909f9ba5abecc9e29c05c8796602132029147
Aldo <aldo.ruiz@wizeline.com>
2020-05-28 10:26:33 -0500

    add js solution

--- src/Easy/ReverseWords/JavaScript/answer.js
+++ src/Easy/ReverseWords/JavaScript/answer.js
@@ -1,7 +1,12 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that reverses the words in a string while keeping word order
+ * @param { string } str - The string to process
+ * @returns { string } - The original string with each word reversed
+ */
+function reverseWords( str ){
+  return str.split(' ').map( word => word.split('').reverse().join('')  ).join(' ')
 }
 
-runTests( myFunction )
+runTests( reverseWords )
