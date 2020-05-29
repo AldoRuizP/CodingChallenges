676475208a69d68fb48d54347cf7a479812f1a8c
Aldo <aldo.ruiz@wizeline.com>
2020-05-29 09:38:11 -0500

    add python solution

--- src/Easy/FindSingleInDuplicates/Python/answer.py
+++ src/Easy/FindSingleInDuplicates/Python/answer.py
@@ -2,8 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def find_single( numbers ):
+  """ A function that scans an array of numbers with duplicated items to find the one item without a duplicate value
 
-run_tests( my_function )
+  Arguments:
+      numbers { num[]} -- An array of numbers
+
+  Returns:
+      num -- A number from the array that has no duplicate
+  """
+  return [ n for n in numbers if numbers.count( n ) == 1 ][0]
+
+run_tests( find_single )
