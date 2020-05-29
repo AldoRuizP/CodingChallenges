52fd645c147299e05646c660cc8dd5494b75c082
Aldo <aldo.ruiz@wizeline.com>
2020-05-29 09:32:06 -0500

    add js solution

--- src/Easy/FindSingleInDuplicates/JavaScript/answer.js
+++ src/Easy/FindSingleInDuplicates/JavaScript/answer.js
@@ -1,7 +1,15 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/** 
+ * A function that scans an array of numbers with duplicated items to find the one item without a duplicate value
+ * Complexity: O(1) + ... + O( 1 ) + O(n) = O(n)
+ * @param { number[] } numbers - An array of numbers
+ * @returns { number } - A number from the array that has no duplicate
+ */
+function findSingleInDuplicates( numbers ){
+  const numbersSet = new Set()
+  numbers.forEach( number =>  numbersSet.has( number ) ? numbersSet.delete( number ) : numbersSet.add( number ) )
+  return Array.from( numbersSet ).pop()
 }
 
-runTests( myFunction )
+runTests( findSingleInDuplicates )
