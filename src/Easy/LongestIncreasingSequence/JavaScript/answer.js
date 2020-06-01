4c0a11d5c10f7e42f0c5e49240108b47ad407f30
Aldo <aldo.ruiz@wizeline.com>
2020-06-01 09:47:44 -0500

    add js solution

--- src/Easy/LongestIncreasingSequence/JavaScript/answer.js
+++ src/Easy/LongestIncreasingSequence/JavaScript/answer.js
@@ -8,18 +8,16 @@ const runTests = require('../../../RunTests/JavaScript/index')
 function longestIncreasingSubsequence( arr ){
 
   let lengths = new Array(arr.length).fill(1)
-  let longest = 0
 
   for( let i = 1; i < arr.length; i++ ){
     for( let j = 0; j < i; j++ ){
       if ( arr[i] > arr[j] ){
         lengths[i] = Math.max( lengths[i], lengths[j] + 1 )
-        longest = Math.max( lengths[i], longest)
       } 
     }
   }
 
-  return longest
+  return Math.max( ...lengths ) 
 }
 
 runTests( longestIncreasingSubsequence )
