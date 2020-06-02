c4758177751025f3bc124115f0f85277193e7f72
Aldo <aldo.ruiz@wizeline.com>
2020-06-02 10:22:11 -0500

    add js docs

--- src/Easy/DetermineIfNumber/JavaScript/answer.js
+++ src/Easy/DetermineIfNumber/JavaScript/answer.js
@@ -1,5 +1,10 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
+/**
+ * A function that scans a string to determine if it represents a valid number, which could be integer, negative, floating, scientific notation, etc
+ * @param { string } str - The string the scan
+ * @returns { boolean } - A boolean determining if the string represents a valid number
+ */
 function determineIfNumber( str ){
   const regex = new RegExp(/^-?\d*\.?\d+[e?\d+]?\d*$/, 'g')
   return regex.test( str )
