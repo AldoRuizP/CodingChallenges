0cd3392f3d227ed5fbf2e73e6ef5640c2fce7574
Aldo <aldo.ruiz@wizeline.com>
2020-06-02 10:21:59 -0500

    add python solution

--- src/Easy/DetermineIfNumber/Python/answer.py
+++ src/Easy/DetermineIfNumber/Python/answer.py
@@ -2,8 +2,18 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+import re
 
-run_tests( my_function )
+def determine_if_number( str ):
+  """ A function that scans a string to determine if it represents a valid number, which could be integer, 
+  negative, floating, scientific notation, etc
+
+  Arguments:
+      str { string } -- The string to scan
+
+  Returns:
+      boolean -- A boolean determining if the string represents a valid number
+  """
+  return re.search("^-?\d*\.?\d+[e?\d+]?\d*$", str) is not None
+
+run_tests( determine_if_number )
