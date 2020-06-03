712289a0c0b8c434b92cb19c0581c75d002c6fdd
Aldo <aldo.ruiz@wizeline.com>
2020-06-03 10:07:10 -0500

    add python solution

--- src/Easy/JewelsAndStones/Python/answer.py
+++ src/Easy/JewelsAndStones/Python/answer.py
@@ -2,8 +2,16 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def count_jewels( jewels_and_stones ):
+  """ A function that counts the number of jewels, given a list of jewel types and the available stones
 
-run_tests( my_function )
+  Arguments:
+      jewels_and_stones { string[] } -- An array of strings. First string represents the list of jewels, and the second string represents stones available
+
+  Returns:
+      number --  the number of available jewels
+  """
+  [J, S] = jewels_and_stones
+  return sum( s in J for s in S )
+
+run_tests( count_jewels )
