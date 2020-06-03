085eeebee356589f34ef7b004878dea303b09a83
Aldo <aldo.ruiz@wizeline.com>
2020-06-03 10:07:19 -0500

    add js solution

--- src/Easy/JewelsAndStones/JavaScript/answer.js
+++ src/Easy/JewelsAndStones/JavaScript/answer.js
@@ -1,7 +1,13 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that counts the number of jewels, given a list of jewel types and the available stones
+ * @param { string[]} jewelsAndStones - An array of strings. First string represents the list of jewels, and the second string represents stones available
+ * @returns { number } - the number of available jewels
+ */
+function countJewels( jewelsAndStones ){
+  const [ jewels, stones ] = jewelsAndStones
+  return jewels.split('').reduce(  ( myJewels, jewel ) => myJewels + ( stones.match( new RegExp(jewel, "g")) || [] ).length, 0 )
 }
 
-runTests( myFunction )
+runTests( countJewels )
