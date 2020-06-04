5d236dabb8fe1a62ce8b893a7098617f785442ba
Aldo <aldo.ruiz@wizeline.com>
2020-06-04 10:56:31 -0500

    add python solution

--- src/Easy/NumbersSmallerThanEach/JavaScript/answer.js
+++ src/Easy/NumbersSmallerThanEach/JavaScript/answer.js
@@ -1,7 +1,13 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that finds how many items are smaller than each item in a given array
+ * @param { number[] } numbers - An array of numbers
+ * @returns { number[] } - An array with the number of elements smaller than each item in the original array
+ */
+function myFunction( numbers ){
+  const sortedNumbers = numbers.slice().sort( (a,b) => a - b )
+  return numbers.map( num => sortedNumbers.indexOf( num ))
 }
 
 runTests( myFunction )
--- src/Easy/NumbersSmallerThanEach/Python/answer.py
+++ src/Easy/NumbersSmallerThanEach/Python/answer.py
@@ -2,8 +2,21 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def my_function( numbers ):
+  """ A function that finds how many items are smaller than each item in a given array
+
+  Args:
+      numbers (number[] ):  An array of numbers
+
+  Returns:
+      number[]: An array with the number of elements smaller than each item in the original arra
+  """
+
+  counts = []
+  for n in numbers:
+    smaller_numbers =  [ i < n for i in numbers ]
+    counts.append( sum( smaller_numbers) )
+
+  return counts
 
 run_tests( my_function )
