f8a9b4ad4b5a40f1d0b243568a8976c6b6e02086
Aldo <aldo.ruiz@wizeline.com>
2020-06-05 11:27:28 -0500

    add python solution

--- src/Easy/SubstractProductAndSum/Python/answer.py
+++ src/Easy/SubstractProductAndSum/Python/answer.py
@@ -2,8 +2,24 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+import math
 
-run_tests( my_function )
+def product_minus_sum( n ):
+  """ A function that scans each digit of a number then sums then and also multiplies them, and finally returns the substraction of both
+
+  Args:
+      n ( number ): The number to process
+
+  Returns:
+      number: The substraction of the product minus the sum
+  """
+  p = 1
+  s = 0
+  while n > 0:
+    c = n % 10
+    p = p * c
+    s = s + c
+    n = math.floor( n / 10 )
+  return p - s
+
+run_tests( product_minus_sum )
