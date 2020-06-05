61312a43c0d0977f80e0b45272e88346a95e40c7
Aldo <aldo.ruiz@wizeline.com>
2020-06-05 11:27:22 -0500

    add js solution

--- src/Easy/SubstractProductAndSum/JavaScript/answer.js
+++ src/Easy/SubstractProductAndSum/JavaScript/answer.js
@@ -1,7 +1,20 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that scans each digit of a number then sums then and also multiplies them, and finally returns the substraction of both
+ * @param { number } n - The number to process
+ * @returns { number } - The substraction of the product minus the sum
+ */
+function productMinusSum(n) {
+  let product = 1
+  let sum = 0
+  while ( n > 0 ) {
+    const current = n % 10
+    product *= current
+    sum += current
+    n = parseInt(n / 10)
+  }
+  return product - sum
 }
 
-runTests( myFunction )
+runTests(productMinusSum)
