4c2a326bf7436c25d00a15a9686e9ca3f8b170a3
Aldo <aldo.ruiz@wizeline.com>
2020-06-08 10:43:44 -0500

    Add python solution

--- src/Easy/IsPalindromeNumber/Python/answer.py
+++ src/Easy/IsPalindromeNumber/Python/answer.py
@@ -2,8 +2,31 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def is_number_palindrome( num ):
+  """ A function that checks if an integer is also a palindrome, without parsing it to string
 
-run_tests( my_function )
+  Args:
+      num ( number ): An integer
+
+  Returns:
+      boolean: True or false if the given integer is a palindrome
+  """
+  digits = number_to_array( num )
+  return digits == digits[::-1]
+
+def number_to_array( num ):
+  """ Helper function that parses an integer number into an array of numbers
+
+  Args:
+      num ( number ): An integer
+
+  Returns:
+      number[]: The given integer, parsed into a list of digits
+  """
+  digits = []
+  while num != 0:
+    num, digit = divmod( num, 10 )
+    digits.append( digit )
+  return digits
+
+run_tests( is_number_palindrome )
