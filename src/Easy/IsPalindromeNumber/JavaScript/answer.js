31fff2693ba6e7d3a19d3cadd1799ad07b2de206
Aldo <aldo.ruiz@wizeline.com>
2020-06-08 10:21:06 -0500

    add js solution

--- src/Easy/IsPalindromeNumber/JavaScript/answer.js
+++ src/Easy/IsPalindromeNumber/JavaScript/answer.js
@@ -1,7 +1,48 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that checks if an integer is also a palindrome, without parsing it to string
+ * @param { number } number - An integer number
+ * @returns { boolean } - True or false if the given integer is a palindrome
+ */
+function isNumberPalindrome( number ){
+  const digits = getNumberDigits( number )
+  return checkPalindromeArray( digits )
 }
 
-runTests( myFunction )
+/**
+ * Helper function that parses an integer number into an array of numbers
+ * @param { number } number - An integer number
+ * @returns { number[] } digits - An array of numbers representing each digit of the given number
+ */
+function getNumberDigits( number ){
+
+  const digits = []
+  let dividend = 1
+  let current = number
+
+  while( current >= 1) {
+    digits.push( parseInt( current % 10 ) )
+    dividend *= 10
+    current = number / dividend
+  }
+
+  return digits
+}
+
+
+/**
+ * Helper function that checks if a given array is palindrome
+ * @param { *[] } arr - An array of items
+ * @returns { boolean } - True or false  if the array is a  palindrome
+ */
+function checkPalindromeArray( arr ){
+  for( let i = 0; i < arr.length / 2; i++ ){
+    const oppositeIndex = arr.length - 1 - i
+    if ( arr[i] !== arr[oppositeIndex] ) return false
+  }
+  return true
+}
+
+
+runTests( isNumberPalindrome )
