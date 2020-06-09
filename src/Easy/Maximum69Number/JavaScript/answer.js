50e56cc159d431c7715b784fd40d62efded07337
Aldo <aldo.ruiz@wizeline.com>
2020-06-09 12:17:48 -0500

    add js solution

--- src/Easy/Maximum69Number/JavaScript/answer.js
+++ src/Easy/Maximum69Number/JavaScript/answer.js
@@ -1,7 +1,13 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that returns the maximum number you can get by changing at most one digit of a given number consisting of 6 and 9
+ * @param { number } num - A number consisting only of 6 and 9
+ * @returns { number } - The highest number that can be made given the input, just by changing one digit
+ */
+function maximum69Number( num ){
+  const adjustedNumber = num.toString().replace( '6', '9')
+  return parseInt( adjustedNumber )
 }
 
-runTests( myFunction )
+runTests( maximum69Number )
