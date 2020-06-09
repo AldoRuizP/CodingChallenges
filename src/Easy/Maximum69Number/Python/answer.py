aa930ba0f2451367cc2d86f93118a0bb26cb6e86
Aldo <aldo.ruiz@wizeline.com>
2020-06-09 12:17:56 -0500

    add python solution

--- src/Easy/Maximum69Number/Python/answer.py
+++ src/Easy/Maximum69Number/Python/answer.py
@@ -2,8 +2,16 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def maximum_69_number( num ):
+  """ A function that returns the maximum number you can get by changing at most one digit of a given number consisting of 6 and 9
 
-run_tests( my_function )
+  Args:
+      num ( number ): A number consisting only of 6 and 9
+
+  Returns:
+      number:  The highest number that can be made given the input, just by changing one digit
+  """
+  adjusted = str(num).replace('6', '9', 1)
+  return int(adjusted)
+
+run_tests( maximum_69_number )
