caafefe78f374e26d7e0391aefd1294e1a964393
Aldo <aldo.ruiz@wizeline.com>
2020-06-12 10:47:44 -0500

    add python solution

--- src/Easy/ProductOfArray/Python/answer.py
+++ src/Easy/ProductOfArray/Python/answer.py
@@ -2,8 +2,26 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def product_of_array( arr ):
+  N = len(arr)
+  res   = [1] * N
+  left  = [1] * N
+  right = [1] * N
 
-run_tests( my_function )
+  product_left = 1
+  product_right = 1
+
+  for i in range( 0, N ):
+    j = N - 1 - i
+    left[i] = product_left 
+    right[j] = product_right
+    product_left *= arr[i]
+    product_right *= arr[j]
+
+  for i in range( 0, N ):
+    res[i] = left[i] * right[i]
+
+  return res
+  
+
+run_tests( product_of_array )
