3610a8cb41b020804ce816306c2f86239e5a2f77
Aldo <aldo.ruiz@wizeline.com>
2020-06-12 10:15:01 -0500

    add js solution

--- src/Easy/ProductOfArray/JavaScript/answer.js
+++ src/Easy/ProductOfArray/JavaScript/answer.js
@@ -1,7 +1,28 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+function productOfArray( arr ){
+
+  const leftSideProducts  = []
+  const rightSideProducts = []
+  const products = []
+  
+  let currentLeftProduct = 1
+  let currentRightProduct = 1
+
+  for( let leftIndex = 0; leftIndex < arr.length; leftIndex++ ){
+    const rightIndex = arr.length - 1 - leftIndex
+    leftSideProducts[ leftIndex ]   = ( currentLeftProduct )
+    rightSideProducts[ rightIndex ] = ( currentRightProduct )
+    currentLeftProduct  *= arr[ leftIndex ]
+    currentRightProduct *= arr[ rightIndex ]
+  }
+
+  for( let i = 0; i < arr.length; i++ ){
+    const currentProduct = leftSideProducts[i] * rightSideProducts[i]
+    products.push( currentProduct )
+  }
+
+  return products
 }
 
-runTests( myFunction )
+runTests( productOfArray )
