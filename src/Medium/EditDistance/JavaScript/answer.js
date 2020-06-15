dd06752241567a0e023fe8012346a0569c506922
Aldo <aldo.ruiz@wizeline.com>
2020-06-15 12:28:28 -0500

    add js solution

--- src/Medium/EditDistance/JavaScript/answer.js
+++ src/Medium/EditDistance/JavaScript/answer.js
@@ -1,7 +1,28 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that calculates the minimum editing distance of two given strings
+ * @param { string[] } strings - An array with two strings to calculate its editing distance
+ * @returns { number } - The editing distance of the two given arrays
+ */
+function editingDistance(strings) {
+
+  const s1 = strings[0]
+  const s2 = strings[1]
+  const temp = Array(s1.length + 1).fill(null).map(() => Array(s2.length + 1).fill(null));
+
+  for (let i = 0; i <= s1.length; i++) {
+    for (let j = 0; j <= s2.length; j++) {
+      if (i === 0 && j === 0) temp[i][j] = 0
+      else if (i === 0) temp[i][j] = temp[i][j-1] + 1
+      else if (j === 0) temp[i][j] = temp[i-1][j] + 1
+      else if ( s1[i-1] == s2[j-1] ) temp[i][j] = temp[i-1][j-1]
+      else temp[i][j] = 1 + Math.min( temp[i-1][j-1], temp[i-1][j], temp[i][j-1] )
+    }
+  }
+
+  return temp[s1.length][s2.length]
 }
 
-runTests( myFunction )
+
+runTests( editingDistance )
