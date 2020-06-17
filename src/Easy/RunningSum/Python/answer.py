2a94a3a0348578bb77d7313dbcea4974ee78ca08
Aldo <aldo.ruiz@wizeline.com>
2020-06-17 09:03:39 -0500

    add python solution

--- src/Easy/RunningSum/Python/answer.py
+++ src/Easy/RunningSum/Python/answer.py
@@ -2,8 +2,18 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+import itertools
 
-run_tests( my_function )
+def running_sum( numbers ):
+  """ A function that given an array, returns the sum of each element until i
+
+  Args:
+      numbers ( number[] ):  An array of integers
+
+  Returns:
+      number[]: An array of integers with the running sum of the given array
+  """
+  return list(itertools.accumulate( numbers ))
+
+
+run_tests( running_sum )
