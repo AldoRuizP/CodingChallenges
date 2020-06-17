f3c01512fa5195ee4b29a8b9b5f048012307273f
Aldo <aldo.ruiz@wizeline.com>
2020-06-17 09:03:46 -0500

    add js solution

--- src/Easy/RunningSum/JavaScript/answer.js
+++ src/Easy/RunningSum/JavaScript/answer.js
@@ -1,7 +1,13 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that given an array, returns the sum of each element until i
+ * @param { number[] } numbers - An array of integers
+ * @returns { number[] } - An array of integers with the running sum of the given array
+ */
+function runningSum( numbers ){
+  let sum = 0
+  return numbers.map( num => sum += num )
 }
 
-runTests( myFunction )
+runTests( runningSum )
