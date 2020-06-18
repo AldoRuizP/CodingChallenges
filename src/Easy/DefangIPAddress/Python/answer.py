1387abe2336741f1d749d725ce36a4f3bca8e605
Aldo <aldo.ruiz@wizeline.com>
2020-06-18 10:24:10 -0500

    add python solution

--- src/Easy/DefangIPAddress/Python/answer.py
+++ src/Easy/DefangIPAddress/Python/answer.py
@@ -2,8 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def defang_address( address ):
+  """ A function that receives an ip address and returns its defanged version
 
-run_tests( my_function )
+  Args:
+      address ( string ): An ip address
+
+  Returns:
+      string: The defanged ip address
+  """
+  return address.replace('.','[.]')
+
+run_tests( defang_address )
