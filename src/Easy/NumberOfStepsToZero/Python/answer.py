be12ffe646b37e7e50578f06659b985ad46f8aad
Aldo <aldo.ruiz@wizeline.com>
2020-06-19 10:08:55 -0500

    add python solution

--- src/Easy/NumberOfStepsToZero/Python/answer.py
+++ src/Easy/NumberOfStepsToZero/Python/answer.py
@@ -2,8 +2,24 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def my_function( n ):
+  """ A function that count the number of needed operations to turn a given number into zero
+
+  Args:
+      n ( integer ): A positive integer
+
+  Returns:
+      integer: A positive integer
+  """
+  steps=0
+
+  while n:
+    if n % 2:
+      n-=1
+    else: 
+      n = n/2
+    steps += 1
+    
+  return steps
 
 run_tests( my_function )
