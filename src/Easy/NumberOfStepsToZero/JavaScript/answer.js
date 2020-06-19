a1857138a11e8a7882adfd423c6fd21c61c56c49
Aldo <aldo.ruiz@wizeline.com>
2020-06-19 10:08:45 -0500

    add js solution

--- src/Easy/NumberOfStepsToZero/JavaScript/answer.js
+++ src/Easy/NumberOfStepsToZero/JavaScript/answer.js
@@ -1,7 +1,17 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that count the number of needed operations to turn a given number into zero
+ * @param { number } num - A positive integer
+ * @returns  { number } steps - A positive integer
+ */
+function numberOfSteps( num ) {
+  let steps = 0
+  while (num) {
+    steps++
+    num % 2 ? num-- : num /= 2
+  }
+  return steps
 }
 
-runTests( myFunction )
+runTests(numberOfSteps)
