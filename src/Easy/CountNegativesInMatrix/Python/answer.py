808ceea7680c058a82014a203145edc11f5231a9
Aldo <aldo.ruiz@wizeline.com>
2020-06-22 14:58:44 -0500

    add python solution

--- src/Easy/CountNegativesInMatrix/Python/answer.py
+++ src/Easy/CountNegativesInMatrix/Python/answer.py
@@ -2,8 +2,22 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def number_of_negatives( grid ):
+  """ A function that returns the number of negative values in a matrix
 
-run_tests( my_function )
+  Args:
+      grid ( number[][] ): An array of arrays on integers
+
+  Returns:
+      number: The total number of negative values in the given matrix
+  """
+
+  negatives = 0
+  for row in grid:
+    for num in row:
+      if num < 0:
+        negatives += 1
+
+  return negatives
+
+run_tests( number_of_negatives )
