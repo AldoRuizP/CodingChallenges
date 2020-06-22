5859521f523dcd124ae384fa477d8714043968ad
Aldo <aldo.ruiz@wizeline.com>
2020-06-22 14:58:54 -0500

    add js solution

--- src/Easy/CountNegativesInMatrix/JavaScript/answer.js
+++ src/Easy/CountNegativesInMatrix/JavaScript/answer.js
@@ -1,7 +1,21 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+
+/**
+ * A function that returns the number of negative values in a matrix
+ * @param { number[][]} grid - An array of arrays on integers
+ * @returns { number } negatives - The total number of negative values in the given matrix
+ */
+function countNegatives(grid) {
+  let negatives = 0
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] < 0) negatives++
+    }
+  }
+
+  return negatives
 }
 
-runTests( myFunction )
+runTests(countNegatives)
