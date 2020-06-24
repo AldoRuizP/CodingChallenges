a8b2faa1c4d4263ef8a24881792a155b7872ad9b
Aldo <aldo.ruiz@wizeline.com>
2020-06-24 14:30:20 -0500

    add js solution

--- src/Easy/KidWithExtraCandies/JavaScript/answer.js
+++ src/Easy/KidWithExtraCandies/JavaScript/answer.js
@@ -1,7 +1,16 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that for each kid check if there is a way to distribute extraCandies among 
+ * the kids such that he or she can have the greatest number of candies among them
+ * @param { number[] } arr - An array representing the candies each kid has, and the last element being the amount of extra candies
+ * @returns { bool[] } - An array determining if each could have the max number of candies, given some extra candies
+ */
+function kidWithExtraCandies( arr ){
+  const extraCandies = arr.pop()
+  const kids = arr
+  const maxCandies = Math.max( ...kids )
+  return kids.map( kid => kid + extraCandies >= maxCandies )
 }
 
-runTests( myFunction )
+runTests( kidWithExtraCandies )
