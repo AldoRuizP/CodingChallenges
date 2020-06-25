72e6d3c2e9892463fddcdda486217dde9a91cfcd
Aldo <aldo.ruiz@wizeline.com>
2020-06-25 12:15:27 -0500

    add js solution

--- src/Easy/DecompressEncodedList/JavaScript/answer.js
+++ src/Easy/DecompressEncodedList/JavaScript/answer.js
@@ -1,7 +1,19 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that decompressed a run-length encoded lists and returns it
+ * @param { number[] } nums - A list compressed with run-length encoding
+ * @returns { number[]  } decompressed - The list after being decompressed
+ */
+function DecompressEncodedList( nums ){
+  let decompressed = []
+  for(let i = 0; i < nums.length; i+=2){
+    const freq = nums[i]
+    const val = nums[i+1]
+    const temp = new Array( freq ).fill( val )
+    decompressed.push( ...temp )
+  } 
+  return decompressed
 }
 
-runTests( myFunction )
+runTests( DecompressEncodedList )
