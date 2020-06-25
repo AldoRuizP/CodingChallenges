ace3b5bd68ab5c5dac82425e5c1143736e7d5b37
Aldo <aldo.ruiz@wizeline.com>
2020-06-25 12:15:15 -0500

    add python solution

--- src/Easy/DecompressEncodedList/Python/answer.py
+++ src/Easy/DecompressEncodedList/Python/answer.py
@@ -2,8 +2,22 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def decompress_encoded_list( nums ):
+  """ A function that decompressed a run-length encoded lists and returns it
 
-run_tests( my_function )
+  Args:
+      nums ( number[] ): A list compressed with run-length encoding
+
+  Returns:
+      number[]: The list after being decompressed
+  """
+  decompressed = []
+
+  for i in range( 0, len(nums), 2 ):
+    freq = nums[i]
+    val = nums[i+1]
+    decompressed.extend( [val] * freq )
+
+  return decompressed
+
+run_tests( decompress_encoded_list )
