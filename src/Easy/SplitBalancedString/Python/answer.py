36510f3e221e66b9f5a772ff0945f3162a614875
Aldo <aldo.ruiz@wizeline.com>
2020-06-26 12:05:27 -0500

    add python slution

--- src/Easy/SplitBalancedString/Python/answer.py
+++ src/Easy/SplitBalancedString/Python/answer.py
@@ -2,8 +2,16 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def maximum_balanced_strings( s ):
 
-run_tests( my_function )
+  count = 0
+  maximum_strings = 0
+
+  for char in s:
+    count +=1 if char == 'L' else -1
+    if count == 0:
+      maximum_strings += 1
+
+  return maximum_strings
+
+run_tests( maximum_balanced_strings )
