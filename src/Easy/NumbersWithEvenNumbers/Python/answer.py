b512851569f8fb7a0c2769fbb6906bd15815fa43
Aldo <aldo.ruiz@wizeline.com>
2020-06-29 12:00:24 -0500

    fix testS

--- src/Easy/NumbersWithEvenNumbers/Python/answer.py
+++ src/Easy/NumbersWithEvenNumbers/Python/answer.py
@@ -2,8 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def numbers_with_even_digits( nums ):
+  """ A function that calculates how many of the numbers in an array contain an even number of digits. 
 
-run_tests( my_function )
+  Args:
+      nums ( number []): An array of numbers
+
+  Returns:
+      number:  The amount of numbers with even digits
+  """
+  return sum(len(str(n)) % 2 == 0 for n in nums)
+
+run_tests( numbers_with_even_digits )
