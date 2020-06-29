95c1568776e02b1f6383232c7eb532ef74ec5e40
Aldo <aldo.ruiz@wizeline.com>
2020-06-29 11:53:57 -0500

    add js solution

--- src/Easy/NumbersWithEvenNumbers/JavaScript/answer.js
+++ src/Easy/NumbersWithEvenNumbers/JavaScript/answer.js
@@ -1,7 +1,12 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that calculates how many of the numbers in an array contain an even number of digits. 
+ * @param { number[] } nums - An array of numbers
+ * @returns { number } - The amount of numbers with even digits
+ */
+function myFunction( nums ){
+  return nums.filter(num => num.toString().length % 2 === 0).length
 }
 
 runTests( myFunction )
