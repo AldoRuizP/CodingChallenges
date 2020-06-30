6add480468be2c11d26f042c4a4ca9c8bd5b2ecb
Aldo <aldo.ruiz@wizeline.com>
2020-06-30 14:05:37 -0500

    add js solution

--- src/Easy/MinTimeToVisitAllPoints/JavaScript/answer.js
+++ src/Easy/MinTimeToVisitAllPoints/JavaScript/answer.js
@@ -1,7 +1,20 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that finds the minimum time in seconds to visit all points in a given array of coordinates
+ * @param { number[] } points - An array of points in a plain
+ * @returns { number } seconds - The amount of seconds needed to go from the first point to the last
+ */
+function minTimeToVisitAllPoints(points) {
+  let seconds = 0
+
+  for (let i = 1; i < points.length; i++) {
+    const diffX = Math.abs(points[i][0] - points[i - 1][0])
+    const diffY = Math.abs(points[i][1] - points[i - 1][1])
+    seconds += Math.max(diffX, diffY)
+  }
+
+  return seconds
 }
 
-runTests( myFunction )
+runTests(minTimeToVisitAllPoints)
