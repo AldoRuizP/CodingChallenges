8d5f656b4e01715e4a1e7174c754fc31a9598ee4
Aldo <aldo.ruiz@wizeline.com>
2020-07-01 11:29:04 -0500

    add python solution

--- src/Easy/MaxProductInArray/Python/answer.py
+++ src/Easy/MaxProductInArray/Python/answer.py
@@ -3,6 +3,15 @@ sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
 def max_product( nums ):
+  """ A function that given the array of integers nums, 
+  returns the maximum value that can be obtain for multiplying two different elements in the array
+
+  Args:
+      nums ( number[] )
+
+  Returns:
+      number
+  """
   n1 = max( nums )
   nums.pop( nums.index( n1) )
   n2 = max( nums )
