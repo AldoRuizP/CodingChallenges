a729356a3d04490b0817ba2e22af2e90af616c52
Aldo <aldo.ruiz@wizeline.com>
2020-07-03 13:10:16 -0500

    add python solution

--- src/Easy/RemoveOuterParentheses/Python/answer.py
+++ src/Easy/RemoveOuterParentheses/Python/answer.py
@@ -2,8 +2,32 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def removeOuterParenthesis( S ):
+  """ A function that given a valid parenthesis string, 
+  removes the outermost parentheses of every primitive string in the primitive decomposition of S.
 
-run_tests( my_function )
+  Args:
+      S (string ): a valid parentheses string 
+
+  Returns:
+      string: the string S the outermost parentheses of every primitive string
+  """
+
+  result_string = ""
+  current_primitive_string = ""
+  current_primitive_stack = []
+
+  for char in S:
+
+    last_char = current_primitive_stack[-1] if current_primitive_stack else char
+    current_primitive_stack.append(char) if last_char == char else  current_primitive_stack.pop()
+
+    if current_primitive_stack:
+      current_primitive_string += char
+    else:
+      result_string += current_primitive_string[1:]
+      current_primitive_string = ""
+  
+  return result_string
+    
+run_tests( removeOuterParenthesis )
