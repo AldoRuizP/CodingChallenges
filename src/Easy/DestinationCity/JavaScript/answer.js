4094fb1c1f2148433ab0682a8a6d4e5a6f0d9bf4
Aldo <aldo.ruiz@wizeline.com>
2020-07-13 12:59:12 -0500

    add js solution

--- src/Easy/DestinationCity/JavaScript/answer.js
+++ src/Easy/DestinationCity/JavaScript/answer.js
@@ -1,7 +1,23 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that given a list of paths, finds a the destination city
+ * @param { string[][] } paths - list of paths, where is path is conformed by origin and destination
+ * @returns { string } destination - the destination city
+ */
+function destinationCities(paths) {
+  const origins = paths.map(path => path[0])
+
+  let destination
+
+  for (let path of paths) {
+    const city = path[1]
+    if (!destination && !origins.includes(city)) {
+      destination = city
+    }
+  }
+
+  return destination
 }
 
-runTests( myFunction )
+runTests(destinationCities)
