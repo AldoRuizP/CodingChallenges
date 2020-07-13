c0ee49323b480e282aff670c29b17527a694bd2b
Aldo <aldo.ruiz@wizeline.com>
2020-07-13 13:33:46 -0500

    add python solution

--- src/Easy/DestinationCity/Python/answer.py
+++ src/Easy/DestinationCity/Python/answer.py
@@ -2,8 +2,25 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def destinationCity( paths ):
+  """ A function that given a list of paths, finds a the destination city
 
-run_tests( my_function )
+  Args:
+      paths ( string[][] ): list of paths, where is path is conformed by origin and destination
+
+  Returns:
+      string: the destination city
+  """
+
+  origins = [ path[0] for path in paths ]
+  destination = None
+
+  for path in paths:
+    city = path[1]
+    if not destination and not city in origins:
+      destination = city
+
+  return destination
+
+
+run_tests( destinationCity )
