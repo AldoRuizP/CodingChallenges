afbf4791b8a0baf43bcc23cc4f499aa59959642a
Aldo <aldo.ruiz@wizeline.com>
2020-07-14 12:50:06 -0500

    add js solution

--- src/Easy/DecryptAlphabet/JavaScript/answer.js
+++ src/Easy/DecryptAlphabet/JavaScript/answer.js
@@ -1,7 +1,28 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that decrypts a message
+ * @param { string } s - Encrypted string
+ * @returns { string } msg - Decrypted message
+ */
+function myFunction(s) {
+
+  const FIRST_ASCII = 96
+  let msg = '';
+
+  for ( let i = 0; i < s.length; i++ ) {
+
+    let encrypted = s[i]
+
+    if( s[i+2] === '#' ){
+      encrypted = s.slice( i, i+2 )
+      i += 2
+    }
+
+    const asciiValue = parseInt(encrypted) + FIRST_ASCII
+    msg += String.fromCharCode(asciiValue)
+  }
+  return msg
 }
 
-runTests( myFunction )
+runTests(myFunction)
