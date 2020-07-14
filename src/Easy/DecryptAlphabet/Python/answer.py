3e720314efd69b3f7f68a3fd9d16080166005f1f
Aldo <aldo.ruiz@wizeline.com>
2020-07-14 13:15:20 -0500

    add python solution

--- src/Easy/DecryptAlphabet/Python/answer.py
+++ src/Easy/DecryptAlphabet/Python/answer.py
@@ -2,8 +2,23 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+FIRST_ASCII = 96
 
-run_tests( my_function )
+def decryptString ( s):
+  msg = ""
+  i = 0
+  
+  while i < len(s):
+    if i+2 < len(s) and s[i+2] == '#':
+      encrypted = s[i:i+2]
+      i += 3
+    else:
+      encrypted = s[i]
+      i += 1
+    
+    asciiValue = int(encrypted) + FIRST_ASCII
+    msg += chr(asciiValue)
+  
+  return msg
+
+run_tests( decryptString )
