cb0dd93c11c40b092599b5294312c2420e1096a5
Aldo <aldo.ruiz@wizeline.com>
2020-07-17 13:02:09 -0500

    add pytghon sohlution

--- src/Easy/ZeroesAndOnes/Python/answer.py
+++ src/Easy/ZeroesAndOnes/Python/answer.py
@@ -2,8 +2,24 @@ import sys
 sys.path.insert(1, sys.path[0] + '/../../../RunTests/Python')
 from run_test import run_tests
 
-def my_function():
-  # Fill this..
-  return
+def zeroesAndOnes( s ):
+  """ A function that removes any pair of adjacent '1' and '0' until no more matches are found, 
+  then return the length of the minimum string
+  
+  Args:
+      s ( string ): A string made out ouf '0', '1' and '*'
 
-run_tests( my_function )
+  Returns:
+      number: The length of the minimal string after applying replacement
+  """
+
+  res = ""
+
+  while res != s:
+    res = s
+    s = s.replace('01','')
+    s = s.replace('10','')
+
+  return len(res)
+
+run_tests( zeroesAndOnes )
