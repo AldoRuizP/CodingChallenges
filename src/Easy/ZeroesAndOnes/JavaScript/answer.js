75a4ee46749c4634f87f5b4cf5aa4546e4f7240c
Aldo <aldo.ruiz@wizeline.com>
2020-07-17 13:02:19 -0500

    add js solution

--- src/Easy/ZeroesAndOnes/JavaScript/answer.js
+++ src/Easy/ZeroesAndOnes/JavaScript/answer.js
@@ -1,7 +1,22 @@
 const runTests = require('../../../RunTests/JavaScript/index')
 
-function myFunction(){
-  /* Fill this... */
+/**
+ * A function that removes any pair of adjacent '1' and '0' until no more matches are found, then return the length of the
+ * minimum string
+ * @param { string } s - A string made out ouf '0', '1' and '*'
+ * @returns { number } s.length - The length of the minimal string after applying replacement
+ */
+function zeroesAndOnes(s) {
+
+  const regex = new RegExp('01|10', 'g')
+  let res
+
+  while (res !== s) {
+    res = s
+    s = s.replace(regex, '')
+  }
+
+  return s.length
 }
 
-runTests( myFunction )
+runTests(zeroesAndOnes)
